d96ef5fa3e313ed0be8bcbb002a619a44911dc76
Developer <dev@timilia.com>
2026-06-10 11:34:27 +0200

    fix: remove static export for Vercel standard deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/sito-chiesa-giubileo',
   images: { unoptimized: true },
-  trailingSlash: true,
 };
 
 export default nextConfig;
